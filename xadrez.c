ad7587b68813dc7ea64926c7ab06c808b0594609
Hizaron <71935787+EvandroPC6@users.noreply.github.com>
2025-04-09 02:46:47 +0000

    NivelNovato

--- xadrez.c
+++ xadrez.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
-// O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
-
 int main() {
-    // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
-    // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
-    // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
-    // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
-
+    int torre = 1, bispo, rainha = 1;
+    //Movimentação do Bispo
+    //Estrutura de repetição "for" para simular a movimentação do Bispo em diagonal.
+    for (bispo = 1; bispo <= 5; bispo++){
+        printf("Bispo cinco casas na diagonal para cima e à direita\n");
+    }
+    //Movimentação da Torre
+    //Estrutura de repetição "while" para simular a movimentação da Torre para a direita.
+    while (torre <= 5){
+        printf("Torre cinco casas para a direita\n");
+        torre++;
+    }
+    //Movimentação da Rainha
+    //Estrutura de repetição "do-while" para simular a movimentação da Rainha para a esquerda.
+    do {
+        printf("Rainha oito casas para a esquerda\n");
+        rainha++;
+    } while (rainha <= 8);
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
