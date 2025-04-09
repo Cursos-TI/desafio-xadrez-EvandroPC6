0dfa80513f655f4e234ac1fe75d166493b2af5c3
Hizaron <71935787+EvandroPC6@users.noreply.github.com>
2025-04-09 05:31:56 +0000

    NivelAventureiro

--- xadrez.c
+++ xadrez.c
@@ -2,15 +2,15 @@
 
 int main() {
 
-    int torre = 1, bispo, rainha = 1;
+    int torre = 0, bispo, rainha = 0, cavalo = 1;
     //Movimentação do Bispo
     //Estrutura de repetição "for" para simular a movimentação do Bispo em diagonal.
-    for (bispo = 1; bispo <= 5; bispo++){
+    for (bispo = 0; bispo < 5; bispo++){
         printf("Bispo cinco casas na diagonal para cima e à direita\n");
     }
     //Movimentação da Torre
     //Estrutura de repetição "while" para simular a movimentação da Torre para a direita.
-    while (torre <= 5){
+    while (torre < 5){
         printf("Torre cinco casas para a direita\n");
         torre++;
     }
@@ -19,11 +19,20 @@ int main() {
     do {
         printf("Rainha oito casas para a esquerda\n");
         rainha++;
-    } while (rainha <= 8);
-    // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
-
+    } while (rainha < 8);
+    //Movimentação do Cavalo
+    //Loops aninhados para simular a movimentação do Cavalo em L.
+    while (cavalo--){
+        for (int movimentoCavalo = 0; movimentoCavalo < 2; movimentoCavalo++){
+            printf("Cavalo duas casas para baixo\n");
+        }
+        printf("Cavalo uma casa para esquerda\n");
+    }
+    
+    
+    {
+        
+    }
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
     // Exemplo: Crie uma função recursiva para o movimento do Bispo.
